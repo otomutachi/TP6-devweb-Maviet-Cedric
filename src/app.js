e1850e29feb70b822bf2ed0b143ab0927d858177
Cédric Maviet <cedric.maviet@gmail.com>
2025-09-29 12:02:00 +1100

    ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const path = require('path');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
-
 
 app.use(express.static(path.join(__dirname, '../static')));
 
@@ -22,7 +24,7 @@ app.use(express.json());
 app.use('/api-v1', require('./routes/api-v1'));
 app.use('/api-v2', require('./routes/api-v2'));
 
-const swaggerDocument = require('../static/open-api.json');
+const swaggerDocument = yaml.load(fs.readFileSync(path.join(__dirname, '../static/open-api.yaml'), 'utf8'));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
